af9cdc522233bcec66b4cbc56f9710a680442bdc
shalom26 <shalom26@gmail.com>
2015-11-30 10:42:55 +0200

    layout

--- public/js/games.js
+++ public/js/games.js
@@ -1,7 +1,16 @@
 // TODO: use ajax to get the file server/games.json
 // Create the list
 
-$.getJSON('api/game', function (games) {
-    console.log(games);
-})
+$.getJSON('api/games', function (games) {
+    var elGamesContainer = $('.container');
+    var strHTML = '';
+    games.forEach(function(game){
+        console.log(game);
 
+        strHTML +=  ' <div class="game ">' +
+            ' <p>'+game.name+'</p>' +
+            ' <img src="'+game.img+'" alt=""/>' +
+            ' </div>'
+    });
+    elGamesContainer.html(strHTML);
+});
